44e6a0b711c7317440e0c0e82df94698baf1a533
Marvish Subhas Chandra <mschandr@uci.edu>
2022-11-17 09:20:03 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,65 @@
+// Marvish Chandra
+
+#include <iostream>
+
+class aisleSeating{
+    public:
+    int RS;
+    int TD;
+    int CL;
+    int S;
+    int EL;
+    int LG;
+    int FB;
+    int FD;
+    int PL;
+    int PR;
+    int BL;
+    int DG;
+    int A;
+    int B;
+    int C;
+    int D;
+    int E;
+    int F;
+    int G;
+    int H;
+    int J;
+    int K;
+    int L;
+    int M; 
+    int N;
+    int P;
+    int VIP;
+};
+
+public static void dsTickets(String [] args){
+    aisleSeating dodgerStadium;
+    dodgerStadium.RS = 150;
+    dodgerStadium.TD = 170;
+    dodgerStadium.CL = 350;
+    dodgerStadium.S = 370;
+    dodgerStadium.EL = 375;
+    dodgerStadium.LG = 400;
+    dodgerStadium.FB = 550;
+    dodgerStadium.FD = 1000;
+    dodgerStadium.PL = 1100;
+    dodgerStadium.PR = 1000;
+    dodgerStadium.BL = 1600;
+    dodgerStadium.DG = 2300;
+    dodgerStadium.A = 550;
+    dodgerStadium.B = 610;
+    dodgerStadium.C = 1180;
+    dodgerStadium.D = 1085;
+    dodgerStadium.E = 818;
+    dodgerStadium.F = 851;
+    dodgerStadium.G = 470;
+    dodgerStadium.H = 573;
+    dodgerStadium.J = 686;
+    dodgerStadium.K = 634;
+    dodgerStadium.L = 850;
+    dodgerStadium.M = 550; 
+    dodgerStadium.N = 555;
+    dodgerStadium.P = 491;
+    dodgerStadium.VIP = 1050;
+}
